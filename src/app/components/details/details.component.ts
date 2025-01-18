9cf9b0c92403b620150bd50e3c288957f630a6c8
agustinservin1 <agustinservin199103@gmail.com>
2025-01-18 11:56:52 -0300

    feat: Implement user data collection form
    Added submitApplication method to HousingService for handling form submissions.
    Imported Angular form classes (FormControl, FormGroup, ReactiveFormsModule) in DetailsComponent.
    Created a reactive form object in DetailsComponent with fields for firstName, lastName, and email.
    Added a onSubmit handler in DetailsComponent to process and log form data.
    Integrated form markup into the DetailsComponent template, including bindings and event handling for user input.

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -15,8 +16,23 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation : HousingLocation | undefined;
   
+  formAngular = new FormGroup({
+    firstName: new FormControl(''),
+    lastName : new FormControl(''),
+    email    : new FormControl(''), 
+  })
+  
   constructor() {
+
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  
+  }
+  onSubmit(){
+    this.housingService.submitApplication(
+      this.formAngular.value.firstName ?? '',
+      this.formAngular.value.lastName ?? '',
+      this.formAngular.value.email ?? '',
+    )  
   }
 }
--- src/app/components/housing.service.ts
+++ src/app/components/housing.service.ts
@@ -116,4 +116,9 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation)=>housingLocation.id==id);
   }
+  submitApplication(firstName: string, lastName: string, email: string)
+  {
+    console.log(`Submitting application for ${firstName} ${lastName} (${email})`);
+    
+  }
 }
